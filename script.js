86d924ad8764489348f7ec32702535adea092378
Dinhn17 <nquydinh@yahoo.com>
2024-02-07 15:27:39 +1030

    added productOfLargestTwo

--- script.js
+++ script.js
@@ -31,3 +31,35 @@ var vowelCount = function(str) {
     console.log(result);
     return result;
 };
+
+// Write code to create a function that accepts an array of numbers, finds the largest two numbers, and returns the product of the two
+var productOfLargestTwo = function(arr) {
+    //try 2
+    var result = [];
+    
+    if (arr[0] > arr[1]) {
+        result[0] = arr[1];
+        result[1] = arr[0];
+    } else {
+        result[0] = arr[0];
+        result[1] = arr[1];
+    };
+
+    if (arr.length > 2) {
+        for (let index = 2; index < arr.length; index++) {
+            const element = arr[index];
+            if (element > result[1]) {
+                result[0] = result[1];
+                result[1] = element;
+            } else if (element > result[0]) {
+                result[0] = element;
+            }; 
+        }
+    };        
+    return result[0] * result[1];
+        
+    //try 1
+    // arr.sort(function(a, b) {return b - a});
+    // return arr[0] * arr[1];
+
+};

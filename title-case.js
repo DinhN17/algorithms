760e5516c46af1cef3008dd7a68c385411677904
Dinhn17 <nquydinh@yahoo.com>
2024-02-09 11:09:36 +1030

    adding title case - tuning try 1

--- title-case.js
+++ title-case.js
@@ -1,3 +1,25 @@
 // Write code to create a function takes a string and returns a new string with the first letters of each word capitalized
 
-var titleCase = function(str) {};
+var titleCase = function(str) {
+    
+    
+    // try 1
+    str.trim();
+    if (str.length === 0) {
+        return str;
+    };
+
+    var arr = str.split("");
+
+    arr[0] = arr[0].toUpperCase();
+    for (let index = 1; index < arr.length; index++) {
+        if (arr[index - 1] === " ") {
+            arr[index] = arr[index].toUpperCase();
+            index++;
+        } else {
+            arr[index] = arr[index].toLowerCase();
+        };
+    };
+
+    return arr.join("");
+};

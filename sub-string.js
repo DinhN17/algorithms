1d117245c8b90b4fe3d582cd889ca54e5ed2e2a2
Dinhn17 <nquydinh@yahoo.com>
2024-02-12 14:44:27 +1030

    added find substr in a str try 2

--- sub-string.js
+++ sub-string.js
@@ -1,29 +1,36 @@
 // find substring in a string sourced from https://www.thatjsdude.com/interview/js1.html
 
-// try 1
+// try 2
 function subStringMatch(str, subStr) {
-    // var index = 0;
-    var strLength = str.length;
-    var subStrLength = subStr.length;
-    const length = strLength - subStrLength + 1;
-    var i = 0;
-    var j = 0;
+    
+    return str.indexOf(subStr);
+    
+}
 
-    for (i = 0; i < length; i++) {
-        j = 0;
-        while (j < subStrLength) {
-            if (str[i+j] != subStr[j]) {
-                break;
-            } else {j++};
-        };
+// try 1
+// function subStringMatch(str, subStr) {
+//     // var index = 0;
+//     var strLength = str.length;
+//     var subStrLength = subStr.length;
+//     const length = strLength - subStrLength + 1;
+//     var i = 0;
+//     var j = 0;
 
-        if (j == subStrLength) {
-            return i;
-        };
+//     for (i = 0; i < length; i++) {
+//         j = 0;
+//         while (j < subStrLength) {
+//             if (str[i+j] != subStr[j]) {
+//                 break;
+//             } else {j++};
+//         };
+
+//         if (j == subStrLength) {
+//             return i;
+//         };
         
-    }
-    return -1;
-};
+//     }
+//     return -1;
+// };
 
 console.log(subStringMatch('abbcdabbbbbck', 'ab'));
 console.log(subStringMatch('abbcdabbbbbck', 'bck'));

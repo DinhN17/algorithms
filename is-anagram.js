6745cb9d8725b00093bce1182dfb8d45a1b2b20d
Dinhn17 <nquydinh@yahoo.com>
2024-02-25 23:06:08 +1030

    added try 2 on isAnagram algo

--- is-anagram.js
+++ is-anagram.js
@@ -15,25 +15,39 @@ var isAnagram = function(strA, strB) {
         return obj;
     }
 
+    // if not same str length, they are not anagram each other.
     if (strA.length != strB.length) {
         return false;
     }
     
-    // try 1: count chars in strA and strB then check if each char in strA has appearance != strB
+    // try 2: count appearances of chars in strA and minus it when go through strB => need 2 loops
     var strAChars = countChars(strA);
-    var strBChars = countChars(strB);
-
-    // console.log(strAChars, strBChars);
-
-    for (const char in strAChars) {
-        if (Object.hasOwnProperty.call(strAChars, char)) {
-            if (!strBChars[char] || (strAChars[char] != strBChars[char])) {
-                return false;
-            };
-        };
-    };
-    
+    for (let index = 0; index < strB.length; index++) {
+        const element = strB[index];
+        if (!strAChars[element] || strAChars[element] < 1) {
+            return false;
+        } else {
+            strAChars[element]--;
+        }
+    }
     return true;
+
+    
+    // try 1: count chars in strA and strB then check if each char in strA has appearance != strB => need 3 loops
+    // var strAChars = countChars(strA);
+    // var strBChars = countChars(strB);
+
+    // // console.log(strAChars, strBChars);
+
+    // for (const char in strAChars) {
+    //     if (Object.hasOwnProperty.call(strAChars, char)) {
+    //         if (!strBChars[char] || (strAChars[char] != strBChars[char])) {
+    //             return false;
+    //         };
+    //     };
+    // };
+    
+    // return true;
 };
 
 console.log(isAnagram("abc", "cba")); //expect true
